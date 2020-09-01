87174d042c5d3ba261b462877e87f608dc3e32e8
sourabhraha <sourabhrahafbg@gmail.com>
2020-09-02 00:44:51 +0530

    2nd program
    cpp program for constructor

--- class.cpp
+++ class.cpp
@@ -10,7 +10,7 @@ class Complex{
 			a = x;
 			b = y;
 		}
-	
+
 		Complex add(Complex c){
 			Complex temp;
 			temp.a = a + c.a;
@@ -27,4 +27,27 @@ main(){
 	c2.set_data(5,6);
 	c3 = c1.add(c2);
 	c3.show_data();
+}
+//program of constructor in Cpp
+#include<iostream>
+using namespace std;
+class Complex{
+	private:
+		int a,b;
+	public:
+		Complex(int x, int y){		//Parametrized Constructor
+			a = x;
+			b = y;
+			cout<<"\na="<<a<<"\nb="<<b;
+		}
+		Complex(int k){
+		 	a = k;
+		 	cout<<"\na="<<a;
+		}
+		Complex(){					//Default Constructor
+			a=0;b=0;
+		}
+};
+ main(){
+	Complex c1(3,4),c2(5),c3,c4(3,8);
 }
